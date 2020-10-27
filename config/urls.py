402ce805724d87f5f358644e43ba7f60366cb76e
julioctadeu <julio.c.tadeu@gmail.com>
2020-10-26 21:21:50 -0300

    remoções

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('accounts/', include('accounts.urls', namespace='accounts')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('order/', include('order.urls', namespace='order')),
     path('coupons/', include('coupons.urls', namespace='coupons')),
